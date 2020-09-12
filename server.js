8ce46c6040793c06d1ada5d74577f10dcee66cbf
Polomac <milan.polomac@gmail.com>
2020-09-12 17:52:24 +0200

    Procfile

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const serveStatic = require('serve-static');
 
 // Serve all the files in '/dist' directory
-app.use(express.static('dist'));
+app.use(serveStatic(`${__dirname}/dist`));
 
 
-app.listen(4000, function () {
-  console.log('Example app listening on port 4000!');
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`server started ${port}`);
 });
